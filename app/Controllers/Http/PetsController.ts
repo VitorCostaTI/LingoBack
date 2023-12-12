41e8b7de09259cc6dfdcc7a18bab2ad4be14fbd4
VitorCostaTI <vitortecnologia62@gmail.com>
2023-12-12 12:28:42 -0300

    Pet: CRUD

---
+++ app/Controllers/Http/PetsController.ts
@@ -0,0 +1,46 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Pet from 'App/Models/Pet';
+
+export default class PetsController {
+    public async post({request, response}: HttpContextContract){
+        const body = request.body();
+
+        Pet.create(body);
+
+        response.status(201);
+        return{
+            msg: "PET criado com sucesso!",
+            Pet
+        };
+    }
+
+    public async get({response}: HttpContextContract){
+        response.status(200);
+        return Pet.all;
+    }
+
+    public async getID({params, response}){        
+        response.status(200);
+        return Pet.findOrFail(params.id);
+    }
+
+    public async update({params, response, request}){        
+        const pet = Pet.findOrFail(params.id);
+        const body = request.body();
+
+        (await pet).pet = body.pet
+        (await pet).especie = body.especie
+        (await pet).raca = body.raca
+        
+        response.status(201);
+    }
+
+    public async delete({response, params}: HttpContextContract){
+        const pet = Pet.findOrFail(params.id);
+
+        return{
+            msg: "Pet deletado com sucesso!",
+            pet
+        }
+    }
+}
