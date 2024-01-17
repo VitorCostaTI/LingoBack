513b01cc13079e42615b0c84725c6a8809c05a20
VitorCostaTI <vitortecnologia62@gmail.com>
2024-01-17 01:39:34 -0300

    Adiciona parametro id nas Urls

--- start/routes.ts
+++ start/routes.ts
@@ -45,8 +45,8 @@ Route.group(() => {
   Route.get('clientes', 'ClientesController.get');
   Route.get('clientes/:id', 'ClientesController.getID');
   Route.post('clientes', 'ClientesController.post');
-  Route.put('clientes', 'ClientesController.update');
-  Route.delete('clientes', 'ClientesController.delete');
+  Route.put('clientes/:id', 'ClientesController.update');
+  Route.delete('clientes/:id', 'ClientesController.delete');
 
   /**** Estoque ****/
   Route.get('estoque', 'EstoquesController.get');
@@ -56,29 +56,29 @@ Route.group(() => {
   Route.get('empresas', 'EmpresasController.get');
   Route.get('empresas/:id', 'EmpresasController.getID');
   Route.post('empresas', 'EmpresasController.post');
-  Route.put('empresas', 'EmpresasController.update');
-  Route.delete('empresas', 'EmpresasController.delete');
+  Route.put('empresas/:id', 'EmpresasController.update');
+  Route.delete('empresas/:id', 'EmpresasController.delete');
 
   /**** Produtos ****/
   Route.get('produtos', 'ProdutosController.get');
   Route.get('produtos/:id', 'ProdutosController.getID');
   Route.post('produtos', 'ProdutosController.post');
-  Route.put('produtos', 'ProdutosController.update');
-  Route.delete('produtos', 'ProdutosController.delete');
+  Route.put('produtos/:id', 'ProdutosController.update');
+  Route.delete('produtos/:id', 'ProdutosController.delete');
 
   /**** Pets ****/
   Route.get('pets', 'PetsController.get');
   Route.get('pets/:id', 'PetsController.getID');
   Route.post('pets', 'PetsController.post');
-  Route.put('pets', 'PetsController.update');
-  Route.delete('pets', 'PetsController.delete');
+  Route.put('pets/:id', 'PetsController.update');
+  Route.delete('pets/:id', 'PetsController.delete');
 
   /**** Usuarios ****/
   Route.get('usuarios', 'UsuariosController.get');
   Route.get('usuarios/:id', 'UsuariosController.getID');
   Route.post('usuarios', 'UsuariosController.post');
-  Route.put('usuarios', 'UsuariosController.update');
-  Route.delete('usuarios', 'UsuariosController.delete');
+  Route.put('usuarios/:id', 'UsuariosController.update');
+  Route.delete('usuarios/:id', 'UsuariosController.delete');
 
 }).prefix('/api').middleware('auth');
 
