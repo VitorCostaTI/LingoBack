4d154f0e493594fee9f56a8e58d0e5e050c04a0a
VitorCostaTI <vitortecnologia62@gmail.com>
2024-09-06 03:46:44 -0300

    Auditorias: Validação de API

--- app/Controllers/Http/AuditoriasController.ts
+++ app/Controllers/Http/AuditoriasController.ts
@@ -2,8 +2,12 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Auditoria from 'App/Models/Auditoria'
 
 export default class AuditoriasController {
-    public async get({response}: HttpContextContract){
-        response.status(200);
-        return Auditoria.all();
+    public async get({ response }: HttpContextContract) {
+        try {
+            response.status(200);
+            return Auditoria.all();
+        } catch (error) {
+            return response.status(500).json({ msg: 'Não foi possivel requisitar auditoria', error })
+        }
     }
 }
