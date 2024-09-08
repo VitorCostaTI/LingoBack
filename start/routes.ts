4639b49fa5d8e9b77c3a54d8e88bebe351d3bd46
VitorCostaTI <vitortecnologia62@gmail.com>
2024-09-08 02:01:06 -0300

    Instalação do Edge && Criação do HomePage

--- start/routes.ts
+++ start/routes.ts
@@ -6,15 +6,18 @@ import Route from '@ioc:Adonis/Core/Route'
 |--------------------------------------------------------------------------
 */
 
-Route.get('/', async () => {
-  return {
-    Welcome: 'Seja bem vindo ao servidor back-end Lingo Kennel',
-    Tecnologia: 'AdonisJS - TypeScript',
-    DevOPS: 'Docker',
-    Database: 'SQL',
-    Desenvolvedor: 'Vitor Costa'
-  }
-});
+// Route.get('/', async () => {
+//   return {
+//     Welcome: 'Seja bem vindo ao servidor back-end Lingo Kennel',
+//     Tecnologia: 'AdonisJS - TypeScript',
+//     DevOPS: 'Docker',
+//     Database: 'SQL',
+//     Desenvolvedor: 'Vitor Costa'
+//   }
+// });
+
+
+Route.get('', 'HomeController.index')
 
 /*
 |--------------------------------------------------------------------------
